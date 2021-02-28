a1a2c04b6a66ef9d814caf322280455a5c93c73f
chisom <iwowochisom@gmail.com>
2021-02-28 16:58:04 +0100

    implement user details

---
+++ src/main/java/com/chisom/transactionservice/model/UserDetailsImpl.java
@@ -0,0 +1,54 @@
+package com.chisom.transactionservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * @author Chisom.Iwowo
+ */
+@AllArgsConstructor
+@Getter
+public class UserDetailsImpl implements UserDetails {
+
+    private final String userName;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
