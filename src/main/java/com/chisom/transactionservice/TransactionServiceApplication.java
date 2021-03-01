4fd6a2aa613fbca4565c602ba27f860f0d373949
chisom <iwowochisom@gmail.com>
2021-03-01 04:12:03 +0100

    add swagger configs

--- src/main/java/com/chisom/transactionservice/TransactionServiceApplication.java
+++ src/main/java/com/chisom/transactionservice/TransactionServiceApplication.java
@@ -3,9 +3,11 @@ package com.chisom.transactionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableSwagger2
 public class TransactionServiceApplication {
 
     public static void main(String[] args) {
